049cd7cfb506f6f6992b45cd8cdc6d0689edee3d
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2023-06-10 23:30:01 +0530

    Add files via upload

---
+++ Guess the Word.py
@@ -0,0 +1,20 @@
+secret_word = " "
+
+#Enter your secret word respectively
+
+guess = " "
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter your Guess : ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("You Lost :( ")
+else:
+    print("You Won :) ")
