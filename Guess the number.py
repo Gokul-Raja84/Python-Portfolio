d9f3c59baf320533167fc0d6450393d60cd7cac1
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2023-06-10 23:29:22 +0530

    Add files via upload

---
+++ Guess the number.py
@@ -0,0 +1,41 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x} : "))
+        if guess < random_number:
+            print("oops Sorry, Try again. Too low")
+        elif guess > random_number:
+            print("oops Sorry, Too High")
+
+    print("You got it. You have guessed the number {random_number} correctly!!")
+
+
+guess(20)
+
+
+# to tell the computer to guess
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+computer_guess(50)
