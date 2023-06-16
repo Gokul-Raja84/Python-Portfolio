4407250c69cb40fb3309491a3b90db607c56bed7
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2023-06-16 12:09:44 +0530

    Github user's Profile Pic

---
+++ Github User Profile/Github Profile Picture.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_user = input ('Input a Github User : ')
+
+url = 'https://github.com/' +github_user
+r = requests.get(url)
+soup = bs(r.content, 'html.parser')
+profile_image = soup.find('img', {'alt' : 'Avatar'})['src']
+print(profile_image)
