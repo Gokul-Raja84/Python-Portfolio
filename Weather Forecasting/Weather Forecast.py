954f7a85d60c99cfd412eb25939f7102e270ce63
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2023-06-17 22:42:02 +0530

    Weather Forecast of a City

---
+++ Weather Forecasting/Weather Forecast.py
@@ -0,0 +1,24 @@
+'''
+
+Weather forecast using Python
+
+'''
+
+#import the necessary Library
+
+import requests
+
+#input the city name
+
+city = input('input the city name')
+
+print('Displaying Weater report for: ' + city)
+
+#To fetch the weather details
+
+url = 'https://wttr.in/{}'.format(city)
+res = requests.get(url)
+
+#resulting weather report
+
+print(res.text)
